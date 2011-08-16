0994398998ecc2f56816b900943851b8e2ddf05c
Chad Metcalf <cm0591@gmail.com>
2011-08-16 16:51:12 -0500

    header
    Finds nearest @event for header alert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :next_event
+  
+  
+  
+private
+  def next_event
+    @next_event = Event.first :conditions => ["date >= ?",Date.today], :order => "date"
+  end
 end
