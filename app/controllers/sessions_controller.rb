d8a0f6d06d95949af543c7cc5d0a786d61d5f60f
Chad Metcalf <chad@databasically.com>
2011-09-02 12:25:42 -0500

    Commented String Title

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     # render :text => request.env["omniauth.auth"]
     begin
-      omniauth['extra']['user_hash']['id'] ?  uid =  omniauth['extra']['user_hash']['id'].to_s : uid = ''
+      # omniauth['extra']['user_hash']['id'] ?  uid =  omniauth['extra']['user_hash']['id'].to_s : uid = ''
       @member = Member.find_or_create_by_auth(request.env["omniauth.auth"])
       session[:member_id] = @member.id
       redirect_to root_path, :notice => "Logged in as #{@member.name}"
