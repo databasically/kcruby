0c9347dab1d43a3c596233a0382a80e25570b7a7
Chad Metcalf <chad@databasically.com>
2011-09-02 10:34:58 -0500

    in progress

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,10 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :twitter, "sDYkn6mYOPMmslawoo5SSg", "2qLjqTOKZuZFd9lyJJvXsV8GZUPypGyhfHzjoRkCb8", options[:authorize_params] = {:force_login => 'true'}
-# end
-
 # begin
 #   settings = YAML.load_file(Rails.root + "config/omniauth.yml")
 # rescue 
 # end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, ENV['TWITTER_KEY'] || settings["key"], ENV['TWITTER_SECRET'] || settings["secret"], :force_login => true
+end
   
-Rails.application.config.middleware.use OmniAuth::Strategies::Twitter, ENV['TWITTER_KEY'] || settings["key"], ENV['TWITTER_SECRET'] || settings["secret"], :force_login => true
+# Rails.application.config.middleware.use OmniAuth::Strategies::Twitter, ENV['TWITTER_KEY'] || settings["key"], ENV['TWITTER_SECRET'] || settings["secret"], :force_login => true
