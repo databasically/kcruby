9c5a950861207d9a823249f079c06fa154e5350d
Chad Metcalf <chad@databasically.com>
2011-09-02 10:05:22 -0500

    catch the login error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,20 @@
 class SessionsController < ApplicationController
   def create
     # render :text => request.env["omniauth.auth"]
-    begin
-      @member = Member.find_or_create_by_auth(request.env["omniauth.auth"])
-      session[:member_id] = @member.id
-      redirect_to root_path, :notice => "Logged in as #{@member.name}"
-    rescue
-      session[:member_id] = nil
-      redirect_to root_path, :notice => "Login Failure"
+    @member = Member.find_or_create_by_auth(request.env["omniauth.auth"])
+    session[:member_id] = @member.id
+    redirect_to root_path, :notice => "Logged in as #{@member.name}"
     end
-  end
   
   def destroy
     session[:member_id] = nil
     redirect_to root_path, :notice => "Logged out"
   end
+  
+  def fail
+    session[:member_id] = nil
+    redirect_to root_path, :notice => "Login Failure"
+  end
+  
+  
 end
