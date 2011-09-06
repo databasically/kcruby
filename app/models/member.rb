7370e03f8bf4a59b5f559906bd0169fffaf5e21f
Chad Metcalf <chad@databasically.com>
2011-09-06 16:04:03 -0500

    nil making problems

--- app/models/member.rb
+++ app/models/member.rb
@@ -11,7 +11,7 @@ class Member < ActiveRecord::Base
       member.avatar = auth_data["user_info"]["image"]
       member.save
     end
-    member.login_count += 1
+    member.login_count += 1 || member.login_count = 1 if member.login_count.blank?
     member.save
     return member
    end
