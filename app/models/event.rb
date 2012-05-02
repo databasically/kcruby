46c9989d634d0c6f0ebf2a304aaecafebb8ed460
Chad Metcalf <chad@databasically.com>
2012-05-02 15:47:32 -0500

    Event MeetUp API Encoding

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
+require 'kconv'
+
 class Event < ActiveRecord::Base
   attr_accessible :name, :event_url, :description, :time, :venue_name, :venue_address1, :venue_address2, :venue_city, :venue_state, :venue_zip, :venue_map
   
@@ -11,12 +13,13 @@ class Event < ActiveRecord::Base
     u = URI.parse("http://api.meetup.com/events.json/?group_urlname=kcruby&amp;key=38a2a70114e565b3c155d919384a12")
     response = Net::HTTP.get(u)
     JSON.parse(response)["results"].each do |ev|
+      # raise ev.inspect
       Event.find_or_create_by_id(ev["id"]).update_attributes(
         :id             => ev["id"],
         :name           => ev["name"],
         :time           => ev["time"],
         :venue_name     => ev["venue_name"],
-        :description    => ev["description"],
+        :description    => ev["description"].toutf8,
         :venue_address1 => ev["venue_address1"],
         :venue_address2 => ev["venue_address2"],
         :venue_city     => ev["venue_city"],
